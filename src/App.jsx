729e4b941c7bc5ac9757850cb2b3317a38fecec0
No.366 <142786224+No-366@users.noreply.github.com>
2025-04-09 16:18:29 +0900

    Idontknow

--- src/App.jsx
+++ src/App.jsx
@@ -227,6 +227,7 @@ const ThermometerIcon = () => (
     <path d="M14 14.76V3.5a2.5 2.5 0 0 0-5 0v11.26a4.5 4.5 0 1 0 5 0z"></path>
   </svg>
 );
+
 const BatteryIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
